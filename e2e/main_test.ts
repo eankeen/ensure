292f6caecdee31f57cc42e70b408ea48e93ea23a
eankeen <24364012+eankeen@users.noreply.github.com>
2020-05-16 00:48:32 -0700

    ci: debug windows

--- e2e/main_test.ts
+++ e2e/main_test.ts
@@ -3,6 +3,28 @@ import { assertEquals } from "https://deno.land/std@v0.50.0/testing/asserts.ts";
 
 const _dirname = path.dirname(new URL(import.meta.url).pathname);
 
+console.log("a", _dirname)
+
+if (Deno.build.os === "windows") {
+  Deno.test({
+  name: "exit code of 0 when running shouldPass",
+  fn: async (): Promise<void> => {
+    const scriptName = path.join(_dirname, "shouldPass.ts").slice(1);
+    const proc = Deno.run({
+      stdout: "piped",
+      stderr: "inherit",
+      stdin: "inherit",
+      cmd: ["deno", "run", scriptName],
+    });
+
+    await proc.output();
+    const status = await proc.status();
+    proc.close();
+
+    assertEquals(status.code, 0);
+  },
+});
+}
 Deno.test({
   name: 'exit code of 0 when running shouldPass',
   fn: async (): Promise<void> => {
